6c42ad730ee8058951015d90397edff006e762a2
Supriya <darshan01075@gail.com>
2025-06-14 19:12:18 +0530

    adding logs

--- src/main/java/com/devops/patient_management/controller/PatientController.java
+++ src/main/java/com/devops/patient_management/controller/PatientController.java
@@ -2,6 +2,7 @@ package com.devops.patient_management.controller;
 
 import com.devops.patient_management.model.Patient;
 import com.devops.patient_management.service.PatientService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.devops.patient_management.metrics.MetricsService;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/patients")
+@Slf4j
 public class PatientController {
 
     @Autowired
@@ -21,6 +23,11 @@ public class PatientController {
     // Get all patients
     @GetMapping
     public List<Patient> getAllPatients() {
+        log.info("info logs getAllPatients");
+        log.debug("debug logs getAllPatients");
+        log.error("error logs getAllPatients");
+        log.warn("warn logs getAllPatients");
+        log.trace("trace logs getAllPatients");
         return patientService.getAllPatients();
     }
 
